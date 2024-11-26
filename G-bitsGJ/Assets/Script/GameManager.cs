be034819db84c786a32da0a11207bb5efc5081e6
forestlyn <yang2797817213@qq.com>
2024-11-26 19:03:15 +0800

    增加GameManager

---
+++ G-bitsGJ/Assets/Script/GameManager.cs
@@ -0,0 +1,58 @@
+﻿
+using UnityEngine;
+
+public class GameManager:MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = Resources.Load<GameObject>("GameManager").GetComponent<GameManager>();
+                instance.Init();
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+            Init();
+        }
+    }
+
+    private PlatformManager platformManager;
+
+    [SerializeField]
+    private float runSpeed = 1.0f;
+    public float RunSpeed
+    {
+        get { return runSpeed; } 
+        set { runSpeed = value; }
+    }
+
+    private void Init()
+    {
+        platformManager = new PlatformManager();
+    }
+
+    private void FixedUpdate()
+    {
+        platformManager.FixedUpdate(Time.fixedDeltaTime * runSpeed);
+    }
+
+    public void Update()
+    {
+        platformManager.Update(Time.deltaTime * runSpeed);
+    }
+}
--- G-bitsGJ/Assets/Script/Platform/BreakablePlatform.cs
+++ G-bitsGJ/Assets/Script/Platform/BreakablePlatform.cs
@@ -34,7 +34,7 @@ public class BreakablePlatform : BasePlatform
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag=="Player")
+        if (collision.transform.tag == "Player")
         {
             beginTimer = true;
         }
